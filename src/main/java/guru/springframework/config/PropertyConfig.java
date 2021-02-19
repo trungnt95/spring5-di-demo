c8fbd676ef853fdd6caf77328842fa2b26da2d1a
Nguyen Thanh Trung <thanhtrungk58uet@gmail.com>
2021-02-19 14:23:43 +0700

    External properties and Enviroment variables

---
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -0,0 +1,68 @@
+package guru.springframework.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+
+import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJmsBroker;
+
+@Configuration
+//@PropertySource({ "classpath:datasource.properties", "classpath:jms.properties" })
+@PropertySources({
+	@PropertySource("classpath:datasource.properties"),
+	@PropertySource("classpath:jms.properties")
+})
+public class PropertyConfig {
+
+	@Autowired
+	Environment env;
+
+	@Value("${guru.username}")
+	String user;
+
+	@Value("${guru.password}")
+	String password;
+
+	@Value("${guru.dburl}")
+	String url;
+
+	@Value("${guru.jms.username}")
+	String jmsUser;
+
+	@Value("${guru.jms.password}")
+	String jmsPassword;
+
+	@Value("${guru.jms.url}")
+	String jmsUrl;
+
+	@Bean
+	public FakeDataSource fakeDataSource() {
+		FakeDataSource fakeDataSource = new FakeDataSource();
+		fakeDataSource.setUrl(url);
+		fakeDataSource.setUser(env.getProperty("USERNAME"));
+		fakeDataSource.setPassword(password);
+
+		return fakeDataSource;
+	}
+
+	@Bean
+	public FakeJmsBroker fakeJmsBroker() {
+		FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+		fakeJmsBroker.setUsername(jmsUser);
+		fakeJmsBroker.setPassword(jmsPassword);
+		fakeJmsBroker.setUrl(jmsUrl);
+		return fakeJmsBroker;
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer properties() {
+		PropertySourcesPlaceholderConfigurer configurer = new PropertySourcesPlaceholderConfigurer();
+		return configurer;
+	}
+}
